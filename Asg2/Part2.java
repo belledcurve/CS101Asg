31c01d4ff10cebd0ee1318cc2edbd8608c6d626e
SAMUEL LEE <samuellee@nyu.edu>
2022-09-28 16:38:36 -0400

    Update Part2.java

--- Asg2/Part2.java
+++ Asg2/Part2.java
@@ -1,4 +1,4 @@
-package Asg2;
+
 /*
 name: Samuel Lee
 date: 9/27/2022
@@ -105,12 +105,17 @@ public class Part2 {
         String tohex = "";
         // revolving hex values by 16
         char[] hexchars = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        while (input != 0)
-        {
+
+        // changed to do-while after finding error where if input == 0 tohex value returns null.
+        do {
             remainder = input % 16;
             tohex = hexchars[remainder] + tohex;
             input /= 16;
         }
+        while (input != 0);
+
+
+
 
         System.out.print(tohex);
 
